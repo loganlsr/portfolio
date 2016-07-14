9038b9d2936b6c2074daa0c5618ec4e650ddde72
Logan Rogers <logan.s.rogers@biola.edu>
2016-07-14 11:19:21 -0700

    added about section and link to project in object

--- JS/projectObjects.js
+++ JS/projectObjects.js
@@ -7,6 +7,6 @@ var projectData = [
     author:       'Logan Rogers',
     projectUrl:  'https://bl41r.github.io/eat_fellows/index.html',
     publishedOn:  '2016-07-08',
-    body:         'This was the final project for the 201 level class at Code Fellows!'
+    body:         'This was the final project for the 201 level class at Code Fellows! </br> <a href="https://bl41r.github.io/eat_fellows/index.html">Click here to check it out!</a>'
   },
 ];
