063dfdfcb2b7a4693a0fb93de07d46f6388752a2
Logan Rogers <logan.s.rogers@biola.edu>
2016-07-15 11:00:20 -0700

    Changed styling, made read more work

--- JS/portfolioView.js
+++ JS/portfolioView.js
@@ -2,17 +2,15 @@
 var portfolioView = {};
 
 portfolioView.setTeasers = function() {
-  $('.article-body *:nth-of-type').hide();
+  $('.article-body *:nth-of-type(n+2)').hide();
   $('.read-on').on('click', function(event) {
     event.preventDefault();
-    event.stopPropagation();
     if($(this).text() === 'Show Less') {
-      $(this).prev().children('*:nth-of-type').hide();
+      $(this).prev().children('*:nth-of-type(n+2)').hide();
       $(this).text('Read More');
     } else {
       event.preventDefault();
-      event.stopPropagation();
-      $(this).prev().children().show();
+      $(this).prev().children('*:nth-of-type(n+2)').show();
       $(this).text('Show Less');
     }
   });
