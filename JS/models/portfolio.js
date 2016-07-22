70b293d264ec2b0a2b3cd6dc23a44cd8194796e0
Logan Rogers <logan.s.rogers@biola.edu>
2016-07-22 09:46:49 -0700

    refactored some more, got rid of unnecessary code, Project not defined?

--- JS/models/portfolio.js
+++ JS/models/portfolio.js
@@ -1,35 +1,5 @@
-'use strict';
-(function(module){
-  // var projects = [];
 
-  // function Project(opts) {
-  //   this.author = opts.author;
-  //   this.title = opts.title;
-  //   this.category = opts.category;
-  //   this.projectUrl = opts.projectUrl;
-  //   this.publishedOn = opts.publishedOn;
-  //   this.body = opts.body;
-  // }
-  //
-  // Project.prototype.toHtml = function(){
-  //   var source = $('#blog-template').html();
-  //   var template = Handlebars.compile(source);
-  //   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' Days Ago';
-  //   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  //   return template(this);
-  // };
-  //
-  // projectData.sort(function(a, b){
-  //   return(new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  // });
-  //
-  // projectData.forEach(function(ele){
-  //   projects.push(new Project(ele));
-  // });
-  // projects.forEach(function(a){
-  //   $('#projects').append(a.toHtml());
-  // });
-// });
+(function(module){
 
   function Projects (opts) {
     for (key in opts) {
--- JS/views/portfolioView.js
+++ JS/views/portfolioView.js
@@ -1,27 +1,40 @@
 'use strict';
+(function(module){
+  var portfolioView = {};
 
-var portfolioView = {};
+  portfolioView.handleNav = function() {
+    $('#top_header button').on('click', function() {
+      $('.dropdown-menu').toggle();
+    });
+  };
 
-portfolioView.handleNav = function() {
-  $('#top_header button').on('click', function() {
-    $('.dropdown-menu').toggle();
-  });
-};
-
-portfolioView.setTeasers = function() {
-  $('.article-body *:nth-of-type(n+2)').hide();
-  $('.read-on').on('click', function(event) {
-    event.preventDefault();
-    if($(this).text() === 'Show Less') {
-      $(this).prev().children('*:nth-of-type(n+2)').hide();
-      $(this).text('Read More');
-    } else {
+  portfolioView.setTeasers = function() {
+    $('.article-body *:nth-of-type(n+2)').hide();
+    $('.read-on').on('click', function(event) {
       event.preventDefault();
-      $(this).prev().children('*:nth-of-type(n+2)').show();
-      $(this).text('Show Less');
-    }
-  });
-};
+      if($(this).text() === 'Show Less') {
+        $(this).prev().children('*:nth-of-type(n+2)').hide();
+        $(this).text('Read More');
+      } else {
+        event.preventDefault();
+        $(this).prev().children('*:nth-of-type(n+2)').show();
+        $(this).text('Show Less');
+      }
+    });
+  };
 
-portfolioView.handleNav();
-portfolioView.setTeasers();
+  portfolioView.renderIndexPage = function() {
+    Project.allProjects.forEach(function(a) {
+      if($('#category-filter option:contains("' + a.category + '")').length === 0) {
+        $('#category-filter').append(a.toHtml($('#category-filter-template')));
+      };
+      if($('#author-filter option:contains("' + a.author + '")').length === 0) {
+        $('#author-filter').append(a.toHtml($('#author-filter-template')));
+      };
+      $('#projects').append(a.toHtml($('#projects-template')));
+    });
+    portfolioView.handleNav();
+    portfolioView.setTeasers();
+  };
+  module.portfolioView = portfolioView;
+})(window);
