61a605868c2ac12de0814015a61d272af82bc371
Logan Rogers <logan.s.rogers@biola.edu>
2016-07-29 14:08:37 -0700

    modified server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var proxyGitHub = function(request, response){
  )(request, response);
 };
 
+app.get('/github/*', proxyGitHub);
+
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
